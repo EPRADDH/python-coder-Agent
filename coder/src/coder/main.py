4fbd794e8594aa50d571c95b8c00eba05f2159f4
Pradeep <pradeep.dhote@quadgenwireless.com>
2025-06-25 19:37:31 +0530

    python coder testes

---
+++ coder/src/coder/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import warnings
+import os
+from datetime import datetime
+
+from coder.crew import Coder
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# Create output directory if it doesn't exist
+os.makedirs('output', exist_ok=True)
+
+assignment = 'Write a python program to scrap the linkdin profiles without linkdin-api'
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'assignment': assignment,
+    }
+    
+    result = Coder().crew().kickoff(inputs=inputs)
+    print(result.raw)
